3d29782cdea975730a8c672e00ef337b3e950215
arghaya <arghayamondal123@gmail.com>
2022-07-06 22:49:13 +0530

    Update automation.sh

--- automation.sh
+++ automation.sh
@@ -6,6 +6,7 @@
 name="arghaya"
 s3_bucket="upgrad-arghaya"
 timestamp=$(date '+%d%m%Y-%H%M%S')
+webroot="/var/www/html"
 
 ## update the repo 
 apt update -y
@@ -22,6 +23,12 @@ fi
 if [[ awscli != $(dpkg --get-selections awscli | awk '{print $1}') ]];
 then
     apt install awscli -y
+fi
+
+## check if git binary is installed or not if not we will install the package automically
+if [[ awscli != $(dpkg --get-selections awscli | awk '{print $1}') ]];
+then
+    apt install git -y
 fi
 
 
@@ -41,11 +48,30 @@ then
 fi
 
 
-
+## archive apache log file and sync it to S3 bucket with date time function
 cd /var/log/apache2
 tar -cvzf /tmp/${name}-httpd-logs-${timestamp}.tar *.log
 
 if [[ -f /tmp/${name}-httpd-logs-${timestamp}.tar ]];
 then
     aws s3 cp /tmp/${name}-httpd-logs-${timestamp}.tar s3://${s3_bucket}/${name}-httpd-logs-${timestamp}.tar
+fi
+
+
+##bookkeeping	
+if [ ! -f ${webroot}/inventory.html ];
+then
+    	printf "<p>Log Type&emsp;Date Created&emsp; &emsp;&emsp; Type&emsp;Size</p>" >${webroot}/inventory.html
+fi
+if [[ -f ${webroot}/inventory.html ]];
+then
+    size=$(du -h /tmp/${name}-httpd-logs-${timestamp}.tar | awk '{print $1}')
+    printf "<p>httpd-logs&emsp;$timestamp&emsp;&emsp;tar&emsp;$size</p>" >> ${webroot}/inventory.html
+fi
+
+
+#add cronjob 
+if [[ ! -f /etc/cron.d/automation ]];
+then
+    echo " * * * * * /root/Automation_Project/automation.sh" >> /etc/cron.d/automation
 fi
